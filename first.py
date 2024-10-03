01b0a40008ec93e2d9eb1432a5559f3a9eb8a966
MattyX204 <cervenkamatyas@gmail.com>
2024-10-03 12:25:31 +0000

    nas prvni program

---
+++ first.py
@@ -0,0 +1,8 @@
+#funkce vypise zda je cislo sude nebo liche
+def sudy_nebo_lichy(cislo):
+    pass
+
+sudy_nebo_lichy(5)
+"Cislo 5 je liche"
+sudy_nebo_lichy(1000000)
+"Cislo 1000000 je sude"
---
+++ pokus.py
@@ -0,0 +1,38 @@
+def hello_world():
+    print("Hello world")
+
+#funkce ktera vypise pozadovany pocet hvezd 
+
+def five_stars(limit):
+    i  = 0
+    while i<limit:
+        print ("*")
+        i += 1
+
+
+#five_stars(10) #cislo 3 se nastavi jako parametr limit
+
+#funkce overi zda je cislo v rozmezi minimum - maximum a vypise textovy vstup
+def in_range(number, minimum, maximum):
+    if(number>minimum and number<maximum):
+        print("Number", number, "is in range 100 - 1000")
+
+    else:
+        print("Number", number, "is out of range") 
+
+
+#in_range(560, 100, 1000)
+
+
+#funkce vrati nejvetsi cislo
+def max_number(a, b, c):
+    if (a>b and a>b):
+        return a 
+    if  (b>a and b>c):
+        return b
+    return c
+    
+
+
+m = max_number(1, 2, 3)
+print(m)
