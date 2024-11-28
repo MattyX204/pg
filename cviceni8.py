583587f58c9c508af2a5b12b95903969f6f3307c
MattyX204 <cervenkamatyas@gmail.com>
2024-11-28 13:22:19 +0000

    cviceni8

---
+++ cviceni8.py
@@ -0,0 +1,42 @@
+def obvod_ctverce(delka_strany):
+    # funkce vypocita obvod ctverce z delky jeho strany
+    return delka_strany*4
+
+
+def obsah_ctverce(delka_strany):
+    # funkce vypocita obsah ctverce z delky jeho strany
+    return delka_strany**2
+
+
+
+def pocet_pismen(text, pismeno):
+    # funkce vrati pocet vyskytu pismene v textu
+    pocet = 0
+    for p in text:
+        if p == pismeno:
+            pocet +=1
+    return pocet
+
+
+def index_pismene(text, pismeno):
+    # funkce vrati indexy daneho pismene v textu, tzn. pro text="ahoj, honzo" a pismeno="h" vrati [1, 6]
+    indexy = []
+    for i,p in enumerate(text):
+        if p == pismeno:
+            indexy.append(i)
+    return indexy
+
+
+def fibonachi(maximum):
+    # funkce vrati fibonachiho posloupnost, pro maximum=5 -> [1, 1, 2, 3, 5]
+    fib = [1,1]
+    soucet = fib[-2]+fib[-1]
+    while soucet <= maximum:
+        fib.append(soucet)
+        soucet = fib[-2]+fib[-1]
+    return fib
+
+
+if __name__ == "__main__":
+    index_pismene("ahoj, honzo", "h")
+    fibonachi(5)
